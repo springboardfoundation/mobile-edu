d1b4a30465a4e53e4972b230273762114fe1d987
unknown <NTH763@NTH763LT-02.home.dsh.com>
2013-12-27 22:43:09 +0530

    Adding SQL for creating DB

---
+++ sql/MessageAsset.sql
@@ -0,0 +1,20 @@
+spool MessageAsset.lst
+
+CREATE TABLE MESSAGE_ASSET
+(
+  MSG_ID INT, /* Internal primary key field */
+  MESSAGE VARCHAR(500) NOT NULL, /* Main content - SMS to be sent */
+  MSG_STD VARCHAR(10), /* Is it for 10th, PUC-1, PUC-2 */
+  INSERT_DATE TIMESTAMP NOT NULL, /* Date when the SMS was inserted */
+  SENT_DATE TIMESTAMP, /* Date when the SMS was broadcasted */
+  IS_ACTIVE BOOLEAN DEFAULT TRUE, /* Added to make it easier to handle SMS that needs to be sent */
+)
+/
+
+ALTER TABLE MESSAGE_ASSET
+ADD CONSTRAINT pk_MsgID PRIMARY KEY (MSG_ID)
+/
+
+COMMIT;
+
+spool off;
---
+++ sql/UserContext.sql
@@ -0,0 +1,20 @@
+﻿spool UserContext.lst
+
+CREATE TABLE USER_CONTEXT
+(
+  CONTEXT_ID INT, /* Internal primary key field */
+  MOBILE_HASH VARCHAR(50) NOT NULL, /* Mobile number hash as received */
+  REG_STD  VARCHAR(10), /* Registered for 10th, PUC-1, PUC-2 */
+  REG_DATE TIMESTAMP NOT NULL, /* Date when the user record was inserted */
+  IS_ACTIVE BOOLEAN DEFAULT TRUE, /* TRUE-Active, FALSE-Otherwise; user request based */
+  LOCATION VARCHAR(20) /* Additional info, can be received from the provider */
+)
+/
+
+ALTER TABLE USERCONTEXT
+ADD CONSTRAINT pk_ContextID PRIMARY KEY (CONTEXT_ID)
+/
+
+COMMIT;
+
+spool off;
